42cb29c7f388317899b8996cf915c7ec9f930864
Alicia Woitte <aliciawoitte@gmail.com>
2016-08-07 09:36:43 -0400

    Initial Commit

---
+++ DiscordDiceBot/MessageHandling.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NCalc;
+
+namespace DiscordDiceBot
+{
+    public static class MessageHandling
+    {
+        public const string rollCommand = "/roll";
+        public static Regex getNumberOfDice = new Regex(@"(\d*)d");
+        public static Regex getValueOfDice = new Regex(@"d(\d+)");
+        public static Random rand = new Random();
+
+        public static bool isRollCommand(string message)
+        {
+            return message.StartsWith(rollCommand);
+        }
+
+        public static string handleRoll(string message, bool logging = true)
+        {
+            var expresssion = message.Replace(rollCommand, "");
+            var numDiceMatch = getNumberOfDice.Match(message);
+            var diceValueMatch = getValueOfDice.Match(message);
+
+            if (!numDiceMatch.Success || !diceValueMatch.Success) return "Invalid Format";
+
+            int numDice = 1;
+            if (numDiceMatch.Groups.Count > 1 && numDiceMatch.Groups[1].Value != "")
+                int.TryParse(numDiceMatch.Groups[1].Value, out numDice);
+
+            if (numDice < 0)
+                return "Can't roll negative dice >:)";
+
+            int diceValue = 0;
+
+            if (diceValueMatch.Groups.Count <= 1 || !int.TryParse(diceValueMatch.Groups[1].Value, out diceValue))
+                return "Couldn't determine dice value";
+
+            if (diceValue < 1)
+                return "Dice must have a positive value";
+
+            var output = "Rolling ";
+
+            output += numDice + "d" + diceValue + " : ";
+
+            List<int> results = new List<int>();
+
+            for (int i = 0; i < numDice; i++)
+            {
+                if (i > 0) output += ", ";
+                int result = rand.Next(1, diceValue + 1);
+                results.Add(result);
+                output += result;
+            }
+
+            int sum = results.Aggregate(0, (curr, roll) => curr + roll);
+
+            if (numDice > 1)
+                output += " SUM: " + sum;
+
+            if(message.Length > diceValueMatch.Index + diceValueMatch.Length)
+            {
+                var expressionString = message.Remove(0, diceValueMatch.Index + diceValueMatch.Length);
+                Expression e = new Expression(sum + expressionString);
+                try
+                {
+                    var result = e.Evaluate();
+
+                    if (logging)
+                    {
+                        Console.WriteLine(output);
+                        Console.WriteLine(expressionString);
+                        Console.WriteLine(result);
+                    }
+                    
+                    if (result is int)
+                        return numDice + "d" + diceValue + expressionString + " : " + result.ToString();
+                }
+                catch (Exception)
+                {
+                    output += " !error evaluating expression!";
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ DiscordDiceBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+
+namespace DiscordDiceBot
+{
+    static class Program
+    {
+        private static DiscordClient _client;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() => Start();
+
+        static void Start()
+        {
+            ;
+            string botToken = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "BotToken.txt"));
+
+            _client = new DiscordClient();
+
+            _client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor && MessageHandling.isRollCommand(e.Message.Text))
+                    await e.Channel.SendMessage(MessageHandling.handleRoll(e.Message.Text));
+            };
+
+            _client.ExecuteAndWait(async () => {
+                await _client.Connect(botToken);
+            });
+        }
+    }
+}
